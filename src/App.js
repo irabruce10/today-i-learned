bb5afa1ba9a51860dd3f910608660679f2e18823
Bruce <irabruce10@gmail.com>
2024-02-27 12:19:14 +0100

    add some fx

--- src/App.js
+++ src/App.js
@@ -2,39 +2,39 @@ import { useEffect, useState } from "react";
 import "./style.css";
 import supabase from "./supabase.js";
 
-const initialFact = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
+// const initialFact = [
+//   {
+//     id: 1,
+//     text: "React is being developed by Meta (formerly facebook)",
+//     source: "https://opensource.fb.com/",
+//     category: "technology",
+//     votesInteresting: 24,
+//     votesMindblowing: 9,
+//     votesFalse: 4,
+//     createdIn: 2021,
+//   },
+//   {
+//     id: 2,
+//     text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+//     source:
+//       "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+//     category: "society",
+//     votesInteresting: 11,
+//     votesMindblowing: 2,
+//     votesFalse: 0,
+//     createdIn: 2019,
+//   },
+//   {
+//     id: 3,
+//     text: "Lisbon is the capital of Portugal",
+//     source: "https://en.wikipedia.org/wiki/Lisbon",
+//     category: "society",
+//     votesInteresting: 8,
+//     votesMindblowing: 3,
+//     votesFalse: 1,
+//     createdIn: 2015,
+//   },
+// ];
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -47,14 +47,26 @@ function App() {
 
   const [facts, setNewItem] = useState([]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(function () {
     async function getFacts() {
-      const { data: facts, error } = await supabase.from("facts").select("*");
+      setIsLoading(true);
+      const { data: facts, error } = await supabase
+        .from("facts")
+        .select("*")
+        .order("votesInteresting", { ascending: false })
+        .limit(1000);
 
       setNewItem(facts);
+      setIsLoading(false);
     }
     getFacts();
   }, []);
+
+  function Loader() {
+    return <p className="message">Loading.....</p>;
+  }
 
   function handleToggle(isOpen) {
     setIsOpen((isOpen) => !isOpen);
@@ -115,7 +127,8 @@ function App() {
 
       <main className="main">
         <CategoryFilter />
-        <FactList newItem={facts} />
+
+        {isLoading ? <Loader /> : <FactList newItem={facts} />}
       </main>
     </>
   );
